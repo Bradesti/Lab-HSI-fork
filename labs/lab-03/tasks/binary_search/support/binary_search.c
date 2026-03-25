4ba84276151d60dd22f5225290ae7b0d870ec8dc
Nicu Robert-Cristian <robertnicu07@gmail.com>
2026-03-25 13:14:41 +0200

    Lab 03 - Binary search: Added solution
    Signed-off-by: Nicu Robert-Cristian <robertnicu07@gmail.com>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -8,10 +8,36 @@ int binary_search(int *v, int len, int dest)
 {
 	int start = 0;
 	int end = len - 1;
-	int middle;
+	int middle, ans = -1;
+	goto C0;
 
-	/**
-	 * TODO: Implement binary search
-	 */
-	return -1;
+L0:
+	middle = (start + end) / 2;
+	goto C1;
+L1:
+	start = middle + 1;
+	goto C2;
+L2:
+	end = middle - 1;
+	goto C2;
+L3:
+	ans = middle;
+	goto END;
+C0:
+	if (start <= end) {
+		goto L0;
+	}
+	goto END;
+C1:
+	if (v[middle] < dest) {
+		goto L1;
+	}
+	goto L2;
+C2:
+	if (v[middle] == dest) {
+		goto L3;
+	}
+	goto C0;
+END:
+	return ans;
 }
